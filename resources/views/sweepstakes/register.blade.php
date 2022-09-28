be129fbaae4c8256b2ff3c2f3995b7f5ba10116f
Lucas Souza <lucasrsouzasilva@gmail.com>
2022-09-28 18:13:01 -0300

    register screen

--- resources/views/sweepstakes/register.blade.php
+++ resources/views/sweepstakes/register.blade.php
@@ -4,79 +4,76 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset("css/app.css") }}" rel="stylesheet">
-    <script defer src="{{ asset("js/app.js") }}" ></script>
+    <script defer src="{{ asset("js/app.js") }}"></script>
 
 </head>
 <body class="h-full">
 <div class="min-h-full" x-data>
     <div class="bg-white">
 
-        <div class="flex flex-col mx-auto mt-14 max-w-2xl sm:mt-16 lg:col-span-3 lg:row-span-2 lg:row-end-2 lg:mt-16 lg:max-w-2xl">
+        <div
+            class="flex flex-col mx-auto mt-14 max-w-2xl sm:mt-16 lg:col-span-3 lg:row-span-2 lg:row-end-2 lg:mt-16 lg:max-w-2xl">
             <div class="flex flex-col-reverse">
                 <div class="mt-4">
-                    <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Application UI Icon Pack</h1>
+                    <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                        {{ $sweepstake->title }}
+                    </h1>
 
-                    <h2 id="information-heading" class="sr-only">Product information</h2>
-                    <p class="mt-2 text-sm text-gray-500">Version 1.0 (Updated <time datetime="2021-06-05">June 5, 2021</time>)</p>
+                    <p class="mt-2 text-sm text-gray-500">
+                        Data do sorteio:
+                        <time
+                            datetime="{{ $sweepstake->end_date->format("d/m/Y") }}">{{ $sweepstake->end_date->format("d/m/Y") }}</time>
+                    </p>
                 </div>
             </div>
 
-            <p class="mt-6 text-gray-500">The Application UI Icon Pack comes with over 200 icons in 3 styles: outline, filled, and branded. This playful icon pack is tailored for complex application user interfaces with a friendly and legible look.</p>
+            <p class="mt-6 text-gray-500">
+                {{ $sweepstake->description }}
+            </p>
 
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-2">
-                <button type="button" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">Pay $220</button>
-                <button type="button" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-50 py-3 px-8 text-base font-medium text-indigo-700 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">Preview</button>
-            </div>
-
-            <div class="mt-10 border-t border-gray-200 pt-10">
-                <h3 class="text-sm font-medium text-gray-900">Highlights</h3>
-                <div class="prose prose-sm mt-4 text-gray-500">
-                    <ul role="list">
-                        <li>200+ SVG icons in 3 unique styles</li>
-
-                        <li>Compatible with Figma, Sketch, and Adobe XD</li>
-
-                        <li>Drawn on 24 x 24 pixel grid</li>
-                    </ul>
+            <form action="{{ route("sweepstakes.register", $sweepstake->id) }}"
+                  class="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-2" method="POST">
+                @csrf
+                <div class="col-span-6">
+                    <label for="name" class="block text-sm font-medium text-gray-700">
+                        Seu nome
+                    </label>
+                    <input value="{{ old('name') }}" placeholder="Nome completo" type="text" name="name" id="name"
+                           autocomplete="given-name"
+                           class="mt-1 block w-full rounded-md
+                           @error('name') border-red-300 @else border-gray-300 @enderror
+                           shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    >
+                    @error('name')
+                    <span class="text-xs text-red-300">{{ $message }}</span>
+                    @enderror
                 </div>
-            </div>
 
-            <div class="mt-10 border-t border-gray-200 pt-10">
-                <h3 class="text-sm font-medium text-gray-900">License</h3>
-                <p class="mt-4 text-sm text-gray-500">For personal and professional use. You cannot resell or redistribute these icons in their original or modified state. <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Read full license</a></p>
-            </div>
+                <div class="col-span-6">
+                    <label for="email" class="block text-sm font-medium text-gray-700">
+                        Seu email
+                    </label>
+                    <input value="{{ old('email') }}" placeholder="email@email.com" type="text" name="email" id="email"
+                           autocomplete="given-name"
+                           class="mt-1 block w-full rounded-md
+                           @error('email') border-red-300 @else border-gray-300 @enderror
+                           shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    >
+                    @error('email')
+                    <span class="text-xs text-red-300">{{ $message }}</span>
+                    @enderror
+                </div>
 
-            <div class="mt-10 border-t border-gray-200 pt-10">
-                <h3 class="text-sm font-medium text-gray-900">Share</h3>
-                <ul role="list" class="mt-4 flex items-center space-x-6">
-                    <li>
-                        <a href="#" class="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500">
-                            <span class="sr-only">Share on Facebook</span>
-                            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500">
-                            <span class="sr-only">Share on Instagram</span>
-                            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500">
-                            <span class="sr-only">Share on Twitter</span>
-                            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+                <div class="w-full bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit"
+                            class=" w-full inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Participar
+                    </button>
+                </div>
+            </form>
+
+
         </div>
-
 
 
     </div>
